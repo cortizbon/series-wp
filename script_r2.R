deb79c74e6946263b3bd883656f2fcc9d640831f
Carlos Ortiz <carlosortizbon@gmail.com>
2024-11-15 17:44:52 -0500

    updating written info

---
+++ script_r2.R
@@ -0,0 +1,35 @@
+# cargar librerías
+library(readr)
+library(reshape2)
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
+library(moments)
+library(tseries)
+library(ConnectednessApproach)
+library(zoo)
+library(kableExtra)
+library(vars)
+
+# cargar datos
+
+
+# graficar series
+
+# estadísticos
+
+# verificar si son estacionarias
+
+# information criteria
+
+# tabla general
+
+# gráficas de dinámicos
+
+# tabla por región
+
+# gráficas de dinámicos
+
+# tabla de potencia
+
+# gráficas de dinámicos
