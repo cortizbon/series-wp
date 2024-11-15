7e5da1308040348257de4fea0491ed670760047c
Carlos Ortiz <carlosortizbon@gmail.com>
2024-11-15 12:06:22 -0500

    adding data

---
+++ script_r.R
@@ -0,0 +1,14 @@
+# leer los datos
+
+
+# graficar los datos
+
+# estadístico descriptivo de todos los datos
+
+# pruebas de estacionariedad (KPSS y ADF)
+
+# Diebold-Yilmaz
+
+# Matriz de Diebold-Yilmaz
+
+# Gráficos de Diebold-Yilmaz
